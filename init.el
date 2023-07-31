c866ae4ec1c725512d099d387ea37c609b22ede5
serpatsama <keetarpalkuhs@gmail.com>
2023-08-01 01:13:57 +0530

    Initial barebones config

---
+++ init.el
@@ -0,0 +1,37 @@
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)            ; Disable the menu bar
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized)) ; Start Maximized
+
+(set-face-attribute 'default nil :font "Source Code Pro" :height 110)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package gruvbox-theme)
+(setq custom-safe-themes t) ;; Suppress annoying message
+(load-theme 'gruvbox-dark-medium)
+
+;; Language modes
+(use-package sml-mode)
